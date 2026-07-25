c408f106dff2504c559ecc6775bbb387d5665510
Abel Melchi <abelmelchipolito@hotmail.com>
2026-07-25 11:30:10 -0600

    Primer despliegue profesional

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://melchisoft.com',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  trailingSlash: 'never',
+  build: {
+    format: 'file'
+  }
+});
